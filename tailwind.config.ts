ddfa9787bbd186bdfa917ab6ee9523125d55e470
Hyziri <hyziri@protonmail.com>
2025-09-19 12:51:21 -0500

    Fix: fix wrong theme being displayed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,22 +2,6 @@
 export default {
   mode: "all",
   content: ["./src/**/*.{rs,html,css}", "./dist/**/*.html"],
-  daisyui: {
-    themes: [
-      {
-        "autumn-light": {
-          primary: "#9a3412",
-          secondary: "#7f1d1d",
-          accent: "#1c1917",
-          neutral: "#000000",
-          "base-100": "#ffffff",
-          info: "#0ea5e9",
-          success: "#22c55e",
-          warning: "#eab308",
-          error: "#ef4444",
-        },
-      },
-    ],
-  },
+  daisyui: {},
   plugins: [],
 };
