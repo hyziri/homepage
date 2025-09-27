984a9513870b11e3212093748d82491ea6b46d81
Hyziri <hyziri@protonmail.com>
2025-09-27 16:27:05 -0500

    fix: remove ID of deleted corporation from stats fetching and fix outdated log message

--- src/api/task/stats.rs
+++ src/api/task/stats.rs
@@ -3,12 +3,11 @@ use sea_orm::DatabaseConnection;
 use crate::api::{constant::APP_VERSION_INFO, service::stats::update_corporation_stats};
 
 const fn get_autumn_corporation_ids() -> &'static [i64] {
-    &[98785281, 98784256]
+    &[98785281]
 }
 
 pub async fn task_update_corporation_stats(db: &DatabaseConnection) {
-    let contact_email =
-        std::env::var("CONTACT_EMAIL").expect("ESI_CONTACT_EMAIL is not set in .env");
+    let contact_email = std::env::var("CONTACT_EMAIL").expect("CONTACT_EMAIL is not set in .env");
 
     let user_agent = format!("{} ({})", APP_VERSION_INFO, contact_email);
     let esi_client = eve_esi::Client::new(&user_agent).unwrap();
