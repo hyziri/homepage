7bab9d52b73f4639d67a8651a88ccae33e44a6fc
Hyziri <hyziri@protonmail.com>
2025-09-28 01:40:37 -0500

    feat: add documentation to Dto fields and struct, add ID to StatsDto

--- src/model/api.rs
+++ src/model/api.rs
@@ -2,6 +2,8 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 #[cfg_attr(feature = "server", derive(utoipa::ToSchema))]
+/// The response when an error occurs with an API request
 pub struct ErrorDto {
+    /// The error message
     pub error: String,
 }
--- src/model/stats.rs
+++ src/model/stats.rs
@@ -3,15 +3,22 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, PartialEq)]
 #[cfg_attr(feature = "server", derive(utoipa::ToSchema))]
+/// A daily entry for the member count of an EVE Online corporation
 pub struct StatsDto {
+    /// ID for the stats entry
+    pub id: i32,
+    /// ID for the EVE Online corporation
     pub corporation_id: i64,
+    /// The member count for the entry
     pub member_count: i64,
+    /// The timestamp for the entry
     pub date: DateTime<Utc>,
 }
 
 impl Default for StatsDto {
     fn default() -> Self {
         StatsDto {
+            id: 0,
             corporation_id: 0,
             member_count: 0,
             date: Utc::now(),
