492b161e61bd74aaefe594c252dfd1ccf2b1dda6
Hyziri <hyziri@protonmail.com>
2025-10-12 02:46:28 -0500

    feat: override ACTIVE_AUTUMN_GUIDE_SUBCATEGORY with default as HIGHSEC when going directly to highsec guide URL

--- src/web/components/guides/autumn/layout.rs
+++ src/web/components/guides/autumn/layout.rs
@@ -2,10 +2,18 @@ use dioxus::prelude::*;
 
 use crate::web::{
     components::{guides::autumn::sidebar::AutumnGuideSidebar, Page, Section},
+    constant::guide::ACTIVE_AUTUMN_GUIDE_SUBCATEGORY,
+    model::guide::AutumnGuideSubcategory,
     Route,
 };
 
 pub fn AutumnGuideLayout() -> Element {
+    let path = router().full_route_string();
+
+    if path.contains("/guides/autumn/highsec") {
+        *ACTIVE_AUTUMN_GUIDE_SUBCATEGORY.write() = AutumnGuideSubcategory::HIGHSEC
+    }
+
     rsx! (
         Page {
             Section {
