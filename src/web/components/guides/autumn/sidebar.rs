0a25afe3ae56b86cbfb71ecd6ef395790cae7cd6
Hyziri <hyziri@protonmail.com>
2025-10-14 00:39:02 -0500

    refactor: move nullsec/highsec buttons into own file to be shared to other components

--- src/web/components/guides/autumn/sidebar.rs
+++ src/web/components/guides/autumn/sidebar.rs
@@ -5,7 +5,10 @@
 use dioxus::prelude::*;
 
 use crate::web::{
-    components::guides::{category::GuideCategoryButton, sidebar::GuideSidebarCategory},
+    components::guides::{
+        autumn::button::category::{HighsecGuideCategoryButton, NullsecGuideCategoryButton},
+        sidebar::GuideSidebarCategory,
+    },
     model::guide::{AutumnGuideSubcategory, GuideCategory},
     routes::guides::autumn::{
         highsec::AUTUMN_HIGHSEC_GUIDE_CATEGORIES, nullsec::AUTUMN_NULLSEC_GUIDE_CATEGORIES,
@@ -51,28 +54,6 @@ pub fn AutumnGuideSidebar(
             }
         }
     )
-}
-
-#[component]
-fn NullsecGuideCategoryButton() -> Element {
-    rsx!(GuideCategoryButton {
-        title: "Autumn Nullsec",
-        description: "The Order of Autumn",
-        image: "https://images.evetech.net/corporations/98785281/logo?size=64".to_string(),
-        class: "bg-gradient-to-br from-orange-800 to-amber-800 w-full p-2 text-white rounded",
-        image_div_class: "bg-amber-900 w-16"
-    })
-}
-
-#[component]
-fn HighsecGuideCategoryButton() -> Element {
-    rsx!(GuideCategoryButton {
-        title: "Autumn Highsec",
-        description: "Autumn Inc.",
-        image: "https://images.evetech.net/corporations/98812612/logo?size=64".to_string(),
-        class: "bg-gradient-to-br from-sky-800 to-cyan-800 w-full p-2 text-white rounded",
-        image_div_class: "bg-cyan-900 w-16"
-    })
 }
 
 #[component]
